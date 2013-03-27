fa73c5338637fd5fb0d456af3b7b3b94401b6a17
Nicholas Rutherford <nick.rutherford@gmail.com>
2013-03-27 16:35:27 +0100

    inject failures into fll

--- src/fll.erl
+++ src/fll.erl
@@ -1,4 +1,5 @@
 % fair loss link
+% injects random delays and losses into transmission
 -module(fll).
 
 -export([ uses/0, upon_event/2, start_link/0, stop/0 ]).
@@ -13,13 +14,27 @@ start_link() ->
 stop() ->
   component:stop(?MODULE).
 
-
-upon_event({fll, send, DestinationNodeQ, Msg}, State) ->
-  stack:transmit(DestinationNodeQ, Msg),
+upon_event(init, State) ->
+  %% Sets a seed for random number generation for the life of the process.
+  % generate 12 crypto-safe random bytes, cast to 3 ints, use as seed
+  <<A:32, B:32, C:32>> = crypto:rand_bytes(12),
+  random:seed({A,B,C}),
   State;
 
 upon_event({fll, deliver, SenderNodeQ, Msg}, State) ->
   io:format("fll received message: ~w from ~w~n", [Msg, SenderNodeQ]),
+  State;
+
+upon_event({fll, send, DestinationNodeQ, Msg}, State) ->
+  case random:uniform(10) of
+    1 ->
+      io:format("fll dropping message~n");
+    2 ->
+      % send with delay
+      timer:apply_after(random:uniform(500), stack, transmit, [DestinationNodeQ, Msg]);
+    _ ->
+      stack:transmit(DestinationNodeQ, Msg)
+  end,
   State;
 
 upon_event(Other, State) ->
