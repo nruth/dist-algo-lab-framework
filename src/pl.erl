4c37dc11c603eb611a28eadf5cfb336e38b11331
Nicholas Rutherford <nick.rutherford@gmail.com>
2013-04-23 18:00:23 +0200

    add disabled msg print to pl

--- src/pl.erl
+++ src/pl.erl
@@ -15,6 +15,7 @@ upon_event(init, _) ->
   #state{delivered=sets:new()};
 
 upon_event({pl, send, DestinationNodeQ, Msg}, State) ->
+  %% io:format("pl sending message: ~w to ~w~n", [Msg, DestinationNodeQ]),
   % ask sl to send the msg, but first tag it with a unique identifier
   stack:trigger({sl, send, DestinationNodeQ, {pl, make_ref(), Msg}}),
   State;
@@ -33,8 +34,8 @@ upon_event({sl, deliver, SenderNodeP, {pl, Id, Msg}}, State) ->
   };
 
 %% upon_event({pl, deliver, SenderNodeQ, Msg}, State) ->
-  %% io:format("pl received message: ~w from ~w~n", [Msg, SenderNodeQ]),
-  %% State;
+%%   io:format("pl received message: ~w from ~w~n", [Msg, SenderNodeQ]),
+%%   State;
 
 upon_event(_Other, State) ->
   %% io:format("~w ignoring event ~w~n", [?MODULE, _Other]),
