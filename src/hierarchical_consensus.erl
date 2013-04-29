56c95ce145b2cef568127347eaafe3d6129c0c86
Nicholas Rutherford <nick.rutherford@gmail.com>
2013-04-29 22:31:57 +0200

    heirc concencus and total order broadcast

--- src/hierarchical_consensus.erl
+++ src/hierarchical_consensus.erl
@@ -1,8 +1,12 @@
 -module(hierarchical_consensus).
 
--export([ uses/0, upon_event/2, stop/0 ]).
--record(state,
-  {instance, detectedranks, round, proposal, proposer, delivered, broadcast}
+-export([ uses/0, upon_event/2, stop/0, propose/2 ]).
+-record(state, {
+  instances,
+  detectedranks
+}).
+-record(instance,
+  {instance, round, proposal, proposer, delivered, broadcast}
 ).
 
 uses() -> [beb, p].
@@ -10,79 +14,132 @@ uses() -> [beb, p].
 stop() ->
   component:stop(?MODULE).
 
-upon_event({init, Instance}, _) ->
+
+propose(Value, Instance) ->
+  stack:trigger({?MODULE, Instance, propose, Value}).
+
+% look up existing consensus instance or make a new one from default values
+get_instance_state(Instance, AllState) ->
+  case orddict:find(Instance, AllState#state.instances) of
+    {ok, Val} ->
+      Val;
+    _ ->
+      % create initial state for new consensus instance
+      #instance{
+        instance = Instance,
+        round = 1,
+        proposal = bottom,
+        proposer = 0,
+        delivered = orddict:new(),
+        broadcast = false
+      }
+  end.
+
+% returns all-state with the instance's entry replaced
+replace_state_instance(Instance, State, AllState) ->
+  AllState#state{instances = stateorddict:store(Instance, State, AllState#state.instances)}.
+
+upon_event(init, _) ->
   #state{
-    instance=Instance,
-    detectedranks = sets:new(),
-    round = 1,
-    proposal = bottom,
-    proposer = 0,
-    delivered = orddict:new(),
-    broadcast = false
+    instances=orddict:new(),
+    detectedranks = sets:new()
   };
 
 
-upon_event({p, crash, PCrashed}, State) ->
-  DetectedState = State#state{
-    detectedranks = sets:add_element(PCrashed, State#state.detectedranks)
+upon_event({p, crash, PCrashed}, AllState) ->
+  AllState2 = AllState#state{
+    detectedranks = sets:add_element(
+      rank(PCrashed), AllState#state.detectedranks
+    )
   },
-  check_round(DetectedState);
+  check_round_condition_all_instances(AllState2);
 
 
-upon_event({Instance, propose, V}, State = #state{instance=Instance, proposal=bottom}) ->
-  io:format("~w proposing: ~w~n", [State#state.instance, V]),
-  check_broadcast(State#state{proposal = V});
-
-
-upon_event({beb, deliver, {Instance, decided, V, PFrom}}, State = #state{instance=Instance}) ->
-  R = rank(PFrom),
-  DeliveredState = State#state{delivered = orddict:store(R, true, State#state.delivered)},
-  ProposalState = case (R < rank(node())) and (R > State#state.proposer) of
-    true ->
-      check_broadcast(DeliveredState#state{proposal = V, proposer = R});
-    _else ->
-      DeliveredState
+upon_event({?MODULE, Instance, propose, V}, AllState) ->
+  InstanceState = get_instance_state(Instance, AllState),
+  InstanceState2 = case InstanceState#instance.proposal of
+    bottom ->
+      io:format("~w proposing: ~w~n", [Instance, V]),
+      InstanceState#instance{proposal = V};
+    _ ->
+      InstanceState
   end,
-  check_round(ProposalState);
+  replace_state_instance(Instance, check_broadcast(InstanceState2), AllState);
+
+% received broadcast msg e.g. {consensus, 1, decided, V, P}
+upon_event({beb, deliver, {?MODULE, Instance, decided, V, PFrom}}, AllState) ->
+  R = rank(PFrom),
+  InstanceState = get_instance_state(Instance, AllState),
+  InstanceState2 = InstanceState#instance{delivered = orddict:store(R, true, InstanceState#instance.delivered)},
+  InstanceState3 = case (R < rank(node())) and
+    (R > InstanceState2#instance.proposer) of
+    true ->
+      InstanceState2#instance{proposal = V, proposer = R};
+    _ ->
+      InstanceState2
+  end,
+  NewAllState = replace_state_instance(Instance, InstanceState3, AllState),
+  check_instance_round_condition(Instance, NewAllState);
 
 upon_event(_Other, State) ->
   %% io:format("~w ignoring event ~w~n", [?MODULE, _Other]),
   State.
+
 
 rank(Node) ->
   IndexedNodes = lists:zip(stack:nodes(), lists:seq(1, length(stack:nodes()))),
   {_Val, Idx} = lists:keyfind(Node, 1, IndexedNodes),
   Idx.
 
-check_round(State) ->
-  RoundDelivered = case orddict:find(State#state.round, State#state.delivered) of
+
+
+check_round_condition_all_instances(AllState) ->
+  orddict:map(
+    fun (InstanceState) ->
+      check_broadcast(InstanceState)
+    end,
+    AllState#state.instances
+  ).
+
+
+% returns updated AllState
+check_instance_round_condition(Instance, AllState) ->
+  InstanceState = get_instance_state(Instance, AllState),
+  replace_state_instance(Instance, check_round_condition(InstanceState, AllState#state.detectedranks), AllState).
+
+% Move to next round if appropriate.
+% Calls check-broadcast when round changes.
+% returns instance state
+check_round_condition(InstanceState, Detectedranks) ->
+  RoundDelivered = case orddict:find(InstanceState#instance.round,
+    InstanceState#instance.delivered) of
     {ok, true}  ->
       true;
     _ ->
       false
   end,
-  RoundState = case
-    sets:is_element(State#state.round, State#state.detectedranks) and RoundDelivered of 
+  case sets:is_element(InstanceState#instance.round, Detectedranks) and RoundDelivered of
     true ->
-      State#state{round = State#state.round + 1};
+      check_broadcast(InstanceState#instance{round = InstanceState#instance.round + 1});
     _ ->
-      State
-  end,
-  check_broadcast(RoundState).
+      InstanceState
+  end.
 
-
-check_broadcast(State) ->
+% returns instance state
+check_broadcast(InstanceState) ->
   case
-    ( (State#state.round == rank(node()))
+    ( (InstanceState#instance.round == rank(node()))
       and
-      (State#state.proposal =/= bottom)
+      (InstanceState#instance.proposal =/= bottom)
       and
-      (State#state.broadcast == false)
+      (InstanceState#instance.broadcast == false)
     ) of
     true ->
-      stack:trigger({beb, broadcast, {State#state.instance, decided, State#state.proposal}}),
-      stack:trigger({State#state.instance, decide, State#state.proposal}),
-      State#state{broadcast=true};
-    _else ->
-      State
+      stack:trigger({beb, broadcast, {InstanceState#instance.instance,
+        decided, InstanceState#instance.proposal}}),
+      stack:trigger({?MODULE, InstanceState#instance.instance, decide,
+        InstanceState#instance.proposal}),
+      InstanceState#instance{broadcast=true};
+    _ ->
+      InstanceState
   end.
---
+++ src/tob.erl
@@ -0,0 +1,65 @@
+% consensus-based total order broadcast
+-module(tob).
+
+-export([ uses/0, upon_event/2, stop/0 ]).
+-record(state, {unordered, delivered, round, wait}).
+
+-define(RESEND_PERIOD, 3000).
+
+uses() -> [rb, heirarchical_consensus].
+
+stop() ->
+  component:stop(?MODULE).
+
+% run consensus on unordered set if not already running
+consensus(State) ->
+  case (State#state.unordered =/= sets:new()) and (State#state.wait == false) of
+    true ->
+      heirarchical_consensus:propose(State#state.unordered, State#state.round),
+      State#state{wait=true};
+    false ->
+      State
+  end.
+
+upon_event(init, _) ->
+  #state{
+    unordered=sets:new(),
+    delivered = sets:new(),
+    round = 1,
+    wait = false
+  };
+
+
+upon_event({tob, broadcast, Msg}, _State) ->
+  stack:trigger({rb, broadcast, Msg});
+
+upon_event({rb, deliver, PSender, Msg}, State) ->
+  StateMaybeNewMsg = case sets:is_element(Msg, State#state.delivered) of
+    true ->
+      State;
+    false ->
+      State#state{unordered = sets:add_element({PSender, Msg}, State#state.unordered)}
+  end,
+  consensus(StateMaybeNewMsg);
+
+upon_event({hierarchical_consensus, Round, decide, Decided}, State)
+  when State#state.round == Round ->
+    % sort and tob deliver the set of messages agreed on for this delivery round
+    lists:foreach(
+      fun ({PSender, Msg}) -> stack:trigger({tob, deliver, PSender, Msg}) end,
+      lists:sort(sets:to_list(Decided))
+    ),
+    % see book errata regarding new value of delivered
+    Msgs = sets:map(fun ({_PSender, Msg}) -> Msg end, Decided),
+    State#state{
+      delivered = sets:union(Msgs, State#state.delivered),
+      unordered = sets:remove(Decided, State#state.unordered),
+      round = State#state.round + 1,
+      wait = false
+    };
+
+% base case, for events this module is not interested in
+upon_event(_Other, State) ->
+  %% io:format("~w ignoring event ~w~n", [?MODULE, Other]),
+  State.
+
