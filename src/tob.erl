7d52fe4d4b14a9fa4a5ac67589d79e919942cf66
Nicholas Rutherford <nick.rutherford@gmail.com>
2013-05-02 13:57:24 +0200

    add holdback queue to tob, deliver when able

--- src/tob.erl
+++ src/tob.erl
@@ -2,7 +2,7 @@
 -module(tob).
 
 -export([ uses/0, upon_event/2, stop/0, broadcast/1 ]).
--record(state, {unordered, delivered, round, wait}).
+-record(state, {decided, unordered, delivered, round, wait}).
 
 uses() -> [rb, hierarchical_consensus].
 
@@ -12,20 +12,12 @@ stop() ->
 broadcast(Msg) ->
   stack:trigger({tob, broadcast, Msg}).
 
-% run consensus on unordered set if not already running
-consensus(State) ->
-  case (State#state.unordered =/= ordsets:new()) and (State#state.wait == false) of
-    true ->
-      hierarchical_consensus:propose(State#state.unordered, State#state.round),
-      State#state{wait=true};
-    false ->
-      State
-  end.
 
 upon_event(init, _) ->
   #state{
     unordered= ordsets:new(),
     delivered = ordsets:new(),
+    decided = orddict:new(),
     round = 1,
     wait = false
   };
@@ -44,36 +36,17 @@ upon_event({rb, deliver, PSender, {tob, Id, Msg}}, State) ->
   end,
   consensus(StateMaybeNewMsg);
 
-upon_event({hierarchical_consensus, Round, decide, Decided}, State)
-  when State#state.round == Round ->
-    io:format("TOB receiving consensus result ~w ~w~n", [Round, Decided]),
-    % sort and tob deliver the set of messages agreed on for this delivery round
-    DeliveryList = lists:sort(ordsets:to_list(Decided)),
-    io:format("TOB delivering ~w~n", [DeliveryList]),
-    lists:map(
-      fun ({PSender, _Id, Msg}) -> stack:trigger({tob, deliver, PSender, Msg}) end,
-      DeliveryList
-    ),
-    % see book errata regarding new value of delivered
-    Msgs = lists:map(
-      fun ({_PSender, _Id, Msg}) -> Msg end,
-      DeliveryList
-    ),
-    %% io:format("Updating state~w~n", [State]),
-    AfterDeliveryState = State#state{
-      delivered = ordsets:union(Msgs, ordsets:from_list(State#state.delivered)),
-      unordered = ordsets:subtract(State#state.unordered, Decided),
-      round = State#state.round + 1,
-      wait = false
-    },
-    %% io:format("Updated state~w~n", [AfterDeliveryState]),
-    AfterDeliveryState;
+%% upon_event({hierarchical_consensus, Round, decide, Decided}, State)
+%%   when State#state.round == Round ->
+%%     io:format("TOB receiving consensus result ~w ~w~n", [Round, Decided]),
+
 
 upon_event({hierarchical_consensus, Round, decide, Decided}, State) ->
+  % TODO: can deliver check and past decisions buffer
+  % TODO: or change consensus to barrier-sync on new-round installation with live nodes so round is only ever current round, so that WAIT var actually works!
     io:format("TOB received ~w waiting for ~w ~n", [Round, State#state.round]),
-    % TODO: can deliver check and past decisions buffer
-    % TODO: or change consensus to barrier-sync on new-round installation with live nodes so round is only ever current round, so that WAIT var actually works!
-    State;
+    try_deliver(State#state{decided = orddict:store(Round, Decided, State#state.decided)});
+
 
 upon_event({tob, deliver, PSender, Msg}, State) ->
   io:format("TOB DELIVER ~w from ~w~n", [Msg, PSender]),
@@ -84,3 +57,48 @@ upon_event(_Other, State) ->
   %% io:format("~w ignoring event ~w~n", [?MODULE, Other]),
   State.
 
+
+
+try_deliver(State) ->
+  case orddict:find(State#state.round, State#state.decided) of
+    {ok, Decided} ->
+      %% io:format("TOB Delivering ~w: ~w~n", [State#state.round, State#state.decided]),
+      AfterDeliver = deliver(Decided, State),
+      AfterDeliver2 = AfterDeliver#state{decided = orddict:erase(State#state.round, AfterDeliver#state.decided)},
+      try_deliver(AfterDeliver2);
+    _ ->
+      State
+  end.
+
+deliver(Decided, State) ->
+  % sort and tob deliver the set of messages agreed on for this delivery round
+  DeliveryList = lists:sort(ordsets:to_list(Decided)),
+  %% io:format("TOB delivering ~w~n", [DeliveryList]),
+  lists:map(
+    fun ({PSender, _Id, Msg}) -> stack:trigger({tob, deliver, PSender, Msg}) end,
+    DeliveryList
+  ),
+  % see book errata regarding new value of delivered
+  Msgs = lists:map(
+    fun ({_PSender, _Id, Msg}) -> Msg end,
+    DeliveryList
+  ),
+  %% io:format("Updating state~w~n", [State]),
+  AfterDeliveryState = State#state{
+    delivered = ordsets:union(Msgs, State#state.delivered),
+    unordered = ordsets:subtract(State#state.unordered, Decided),
+    round = State#state.round + 1,
+    wait = false
+  },
+  %% io:format("Updated state~w~n", [AfterDeliveryState]),
+  AfterDeliveryState.
+
+% run consensus on unordered set if not already running
+consensus(State) ->
+  case (State#state.unordered =/= ordsets:new()) and (State#state.wait == false) of
+    true ->
+      hierarchical_consensus:propose(State#state.unordered, State#state.round),
+      State#state{wait=true};
+    false ->
+      State
+  end.
